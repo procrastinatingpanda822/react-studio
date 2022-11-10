acbdb1f5ca1630ac3811daf2351a8503646bb8fa
procrastinatingpanda822 <115194698+procrastinatingpanda822@users.noreply.github.com>
2022-11-10 00:34:00 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
-  item.image = process.env.PUBLIC_URL + "/" + item.image;
+    item.image = process.env.PUBLIC_URL + "/" + item.image;
 });
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+    // TODO: use useState to create a state variable to hold the state of the cart
+    /* add your cart state code here */
 
-  return (
-    <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+    const [cart, setCart] = useState([]);
+    const [totalPrice, updateTotalPrice] = useState(0);
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+    function updateCart(item) {
+        setCart([...cart, item]);
+        updateTotalPrice(totalPrice + item.price);
+    }
 
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
-      </div>
-    </div>
-  );
+    return (
+        <div class="parent">
+            <div class="child1" >
+                <h1>My Bakery</h1>
+                {/* TODO: personalize your bakery (if you want) */}
+                {bakeryData.map(
+                    (
+                        item // TODO: map bakeryData to BakeryItem components
+                    ) => (
+                        <BakeryItem item={item} updateCart={updateCart} />
+                    )
+                )}
+            </div>
+
+            <div class="child2">
+                <h1>Cart</h1>
+                <h3>Total: {totalPrice}</h3>
+                {/* TODO: render a list of items in the cart */}
+                {cart.map((item) => (
+                    <div>Added: {item.name}</div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
